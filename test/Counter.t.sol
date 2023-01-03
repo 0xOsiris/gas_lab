06c82049e72adaec02d4975d01f7cfef2d9f32dd
Leyton <leytontaylor23@gmail.com>
2023-01-02 19:30:13 -0600

    First commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
 import "../src/Counter.sol";
+import "./utils/test.sol";
+import "./utils/Console.sol";
+import "../src/TickBitmap.sol";
 
-contract CounterTest is Test {
+contract CounterTest is DSTest {
     Counter public counter;
+    using TickBitmap for mapping(int16 => uint256);
+    mapping(int16 => uint256) public tickBitmap;
 
     function setUp() public {
+        tickBitmap[
+            80
+        ] = 50196650721032492405623933735829147368684632089292599712538235063225972359167;
         counter = new Counter();
         counter.setNumber(0);
     }
@@ -20,5 +27,12 @@ contract CounterTest is Test {
     function testSetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
+    }
+
+    function testBitmap() public {
+        (int24 next, bool initialized) = TickBitmap
+            .nextInitializedTickWithinOneWord(tickBitmap, 205299, 10, true);
+        console.logInt(next);
+        console.log(initialized);
     }
 }
